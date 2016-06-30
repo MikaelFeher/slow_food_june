1095e6dbf67301bf2fc0f18d950215da26a27b0e
tochman <thomas@craftacademy.se>
2016-06-30 09:28:50 +0200

    refactored step defs after merge with upstream

--- features/step_definitions/dishes_steps.rb
+++ features/step_definitions/dishes_steps.rb
@@ -1,10 +1,3 @@
-Given(/^I am on the home page$/) do
-  visit '/'
-end
-
-And(/^I click "([^"]*)"$/) do |element|
-  click_link_or_button element
-end
 
 Then(/^I should be on the dish display page$/) do
   expect(page.current_path).to eq '/dishes'
