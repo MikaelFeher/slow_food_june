e2b651f50f20f56d6c1a42906d0781dd6ddd18bd
tochman <thomas@craftacademy.se>
2016-06-30 09:30:39 +0200

    refactored step defs after merge with upstream - 2

--- features/step_definitions/dishes_steps.rb
+++ features/step_definitions/dishes_steps.rb
@@ -2,7 +2,3 @@
 Then(/^I should be on the dish display page$/) do
   expect(page.current_path).to eq '/dishes'
 end
-
-Then(/^I should see "([^"]*)"$/) do |content|
-  expect(page).to have_content content
-end
